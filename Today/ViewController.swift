1c3ea14a93ae7d9b9ebe61b1f4b095222c6bd018
JAY <JAY@redminote3-redmi.mshome.net>
2018-03-26 11:04:33 +0530

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by JAY R SONANI on 05/01/1940 Saka.
+//  Copyright © 1940 Saka JAY R SONANI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
